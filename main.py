a0ab9feb4ba342bf506c2c27fd55e98c785d7abc
Zajvyj <dimazajvyj0@gmail.com>
2026-08-27 09:20:22 +0200

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+# Set up the display
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Set up the clock for managing the frame rate
+clock = pygame.time.Clock()
