89317721f1801d28f6fb5d4bf7d4ca8e730b828d
Zajvyj <dimazajvyj0@gmail.com>
2026-08-31 12:49:03 +0200

    Initialized resolutioin scaling

--- main.py
+++ main.py
@@ -3,13 +3,56 @@ import pygame
 pygame.init()
 
 # Set up the display
-screen = pygame.display.set_mode((800, 600))
+RESOLUTIONS = [(800, 600), (1024, 768), (1280, 720), (1366, 768), (1600, 900), (1920, 1080)]
+current_res_idx = 0
+pygame.display.set_caption("Tetris Game")
+
+VIRTUAL_WIDTH = 800
+VIRTUAL_HEIGHT = 600
+
+virtual_surface = pygame.Surface((VIRTUAL_WIDTH, VIRTUAL_HEIGHT))
+
+
+current_w, current_h = RESOLUTIONS[current_res_idx]
+screen = pygame.display.set_mode((current_w, current_h))
+pygame.display.set_caption("Resolution setting")
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 40)
+running = True
+
+def change_resolution(index):
+    global screen, current_w, current_h
+    current_w, current_h = RESOLUTIONS[index]
+    # recreate window with new resolution
+    screen = pygame.display.set_mode((current_w, current_h))
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            # you can set up resolution by using 1, 2, 3, 4, 5, 6
+            if event.key == pygame.K_1:
+                current_res_idx = 0
+                change_resolution(current_res_idx)
+            elif event.key == pygame.K_2:
+                current_res_idx = 1
+                change_resolution(current_res_idx)
+            elif event.key == pygame.K_3:
+                current_res_idx = 2
+                change_resolution(current_res_idx)
+            elif event.key == pygame.K_4:
+                current_res_idx = 3
+                change_resolution(current_res_idx)
+            elif event.key == pygame.K_5:
+                current_res_idx = 4
+                change_resolution(current_res_idx)
+            elif event.key == pygame.K_6:
+                current_res_idx = 5
+                change_resolution(current_res_idx)
 
 # Set up the clock for managing the frame rate
 clock = pygame.time.Clock()
